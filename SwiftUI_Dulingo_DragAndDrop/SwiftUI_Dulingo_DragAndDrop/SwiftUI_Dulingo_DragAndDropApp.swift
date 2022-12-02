41c34dec5f0f6ba731a2cb7a043f4c9292b16fd9
전지훈 <junji636@gmail.com>
2022-12-02 23:48:17 +0900

    first commit

---
+++ SwiftUI_Dulingo_DragAndDrop/SwiftUI_Dulingo_DragAndDrop/SwiftUI_Dulingo_DragAndDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Dulingo_DragAndDropApp.swift
+//  SwiftUI_Dulingo_DragAndDrop
+//
+//  Created by 전지훈 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Dulingo_DragAndDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
