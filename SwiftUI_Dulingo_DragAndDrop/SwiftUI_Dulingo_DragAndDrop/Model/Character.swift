5a508109deabb84ed7d28a91cf514c51c75ba2b8
전지훈 <junji636@gmail.com>
2022-12-03 23:58:32 +0900

    Project Setup

---
+++ SwiftUI_Dulingo_DragAndDrop/SwiftUI_Dulingo_DragAndDrop/Model/Character.swift
@@ -0,0 +1,14 @@
+//
+//  Character.swift
+//
+
+import Foundation
+
+struct Character: Identifiable, Hashable, Equatable {
+    var id = UUID().uuidString
+    var value: String
+    var padding: CGFloat = 10
+    var textSize: CGFloat = .zero
+    var fontSize: CGFloat = 19
+    var isShowing: Bool = false
+}
