ab44675e9ea1dc5e98e2850c48b8978bfb6febbc
전지훈 <junji636@gmail.com>
2022-12-07 00:03:45 +0900

    홈화면 작업중 - progress property 추가

--- SwiftUI_Dulingo_DragAndDrop/SwiftUI_Dulingo_DragAndDrop/View/Home.swift
+++ SwiftUI_Dulingo_DragAndDrop/SwiftUI_Dulingo_DragAndDrop/View/Home.swift
@@ -5,9 +5,17 @@
 import SwiftUI
 
 struct Home: View {
+    //MARK: Properties
+    @State var progress: CGFloat = 0
+    
     var body: some View {
         VStack(spacing: 15) {
             navBar()
+            
+            VStack(alignment: .leading, spacing: 30) {
+                Text("Form This Sentence")
+                    .font(.title2.bold())
+            }
         }
         .padding()
     }
@@ -30,9 +38,21 @@ extension Home {
                     ZStack {
                         Capsule()
                             .fill(.gray.opacity(0.25))
+                        
+                        Capsule()
+                            .fill(.green)
+                            .frame(width: geo.size.width * progress)
                     }
                 }
                 .frame(height: 20)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "suit.heart.fill")
+                        .font(.title3)
+                        .foregroundColor(.red)
+                }
             }
         }
     }
