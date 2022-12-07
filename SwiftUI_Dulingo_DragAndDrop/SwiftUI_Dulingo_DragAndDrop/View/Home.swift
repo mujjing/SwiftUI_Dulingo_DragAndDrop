8d0c893ccd25432cb44e2c7466c9274a9d6246fe
전지훈 <junji636@gmail.com>
2022-12-07 23:49:09 +0900

    drag and drop 영역 작업 시작

--- SwiftUI_Dulingo_DragAndDrop/SwiftUI_Dulingo_DragAndDrop/View/Home.swift
+++ SwiftUI_Dulingo_DragAndDrop/SwiftUI_Dulingo_DragAndDrop/View/Home.swift
@@ -7,6 +7,11 @@ import SwiftUI
 struct Home: View {
     //MARK: Properties
     @State var progress: CGFloat = 0
+    @State var characters: [Character] = characters
+    
+    //MARK: Custom Grid Arrays
+    @State var suffledArray: [[Character]] = [[]]
+    @State var rows: [[Character]] = [[]]
     
     var body: some View {
         VStack(spacing: 15) {
@@ -15,13 +20,29 @@ struct Home: View {
             VStack(alignment: .leading, spacing: 30) {
                 Text("Form This Sentence")
                     .font(.title2.bold())
+                
+                Image("j")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 250, height: 250)
             }
+            .padding(.top, 30)
+            
+            //MARK: Drag And Drop Space
         }
         .padding()
     }
 }
 
 extension Home {
+    func dragArea() -> some View {
+        ZStack {
+            VStack(spacing: 12) {
+                
+            }
+        }
+    }
+    
     //MARK: Custom Nav Bar
     func navBar() -> some View{
         ZStack {
